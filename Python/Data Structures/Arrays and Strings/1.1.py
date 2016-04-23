24e603d08ac90b28f69362f42c75a700cb614d6b
Edward Xue <edthemaster1@gmail.com>
2016-04-22 22:22:24 -0500

    Improved solution 1.1. REMEMBER TO ALWAYS THINK ABOUT MAKING THINGS SMALLER In this case, our data structure does not need to be complicated like a dictionary, but rather an array of O(1) size.

--- Python/Data Structures/Arrays and Strings/1.1.py
+++ Python/Data Structures/Arrays and Strings/1.1.py
@@ -21,6 +21,31 @@ Time Complexity: O(n), since in the worst case we need to look at the entire str
 Space Complexity: O(n), to store all the unqiue chars in our counts dictionary
 '''
 
+#Improved solution (from book): there are only a finite amount of different characters that exist. 
+#So while the string may be arbitrarily large, we only need a O(1) extra space to check if characters are duplicated
+
+#Assume 256 possible unique characters (ASCII)
+#function ord() would get the int value of the char. 
+#And in case you want to convert back after playing with the number, function chr() does the trick.	
+def has_unique_chars_2(str):
+	if len(str) > 256:
+		return False
+	char_set = [False for i in range(256)] # list comprehension, for creating a boolean array all initialized to false
+	for i in range(0, len(str)):
+		char_val = ord(str[i])
+		if(char_set[char_val]):
+			return False
+		char_set[char_val] = True
+	return True
+
+print("=====")	
+print has_unique_chars_2("HIiiH")
+assert has_unique_chars_2("HIiiH") == False
+print has_unique_chars_2("HiI")
+assert has_unique_chars_2("HiI") == True
+	
+
+
 #What if you cannot use additional data structures?
 
 #Have to compare each char with every other char.
