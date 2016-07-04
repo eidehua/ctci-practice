5e67b9992d96d29e5e6bf8b7f088bd41929525b8
eidehua <edthemaster1@gmail.com>
2016-07-03 22:50:46 -0700

    Finished 1.2

--- Java/Data_Structures/Arrays_and_Strings/P_1_2_Check_Permutation.java
+++ Java/Data_Structures/Arrays_and_Strings/P_1_2_Check_Permutation.java
@@ -1,20 +1,24 @@
-package Arrays_and_Strings;// Given two strings, write a method to decide if one is a permutation of the other
-// Check if they are same length
-
-
+package Arrays_and_Strings;
 import java.util.Hashtable;
+// Things to keep in mind I forgot about:
+    // for these permutations, do we care about case sensitivity? And what about whitespaces? Do not make hasty assumptions!
 
+// Given two strings, write a method to decide if one is a permutation of the other
+// Check if they are same length
 public class P_1_2_Check_Permutation{
 
     public static void main(String[] args) {
-        String a = "olehl";
-        String b = "hello";
-        String c = "barry";
+        String a = "olehll";
+        String b = "helllo";
+        String c = "balrry";
+        String d = "helll0";
         boolean result = false;
         result = checkPermutation(a,b);
-        assert  result == true;
+        assert  result;
         result = checkPermutation(b, c);
-        assert result == false;
+        assert !result;
+        result = checkPermutation(b,d);
+        assert !result;
     }
 
 // Method 1: Hash Table
@@ -36,7 +40,6 @@ public class P_1_2_Check_Permutation{
             //currCount++;
             else {
                 counts.put(currChar, ++currCount);
-                System.out.println(currChar + " " + counts.get(currChar));
             }
         }
 
@@ -53,14 +56,47 @@ public class P_1_2_Check_Permutation{
         }
         // since A and B are the same size, passing the above two if checks means A and b are the same
 
+        System.out.println(counts);
         return true;
     }
 
 
 // Runtime: O(n) loop through first string + O(n) loop through second string + O(n) verify hash table = O(n)
 // Space complexity: O(n) extra space for the hash table
+    // note of contention: is it really O(n) extra space for each hash table? There are only a limited number of characters (usually good assumption)
+    // although in such the case, method 2 provides a much cleaner implementation
 
 // Method 2: 
+    // REMEMBER, REMEMBER!! Strings are made up of chars, and usually we don't deal with an INFINITE number of chars!!!
+    // EG if the character set is just ASCII, then to keep track of counts like I did with the hashtable, we only need an array of size 128!
+    // Solution from book:
+    public static boolean permutation(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
 
+        int[] letters = new int[128]; // char set assumption
+
+        char[] a_array = a.toCharArray();
+        for(char c : a_array) { // count the number of each char in a
+            letters[c]++;
+        }
+
+        for(int i = 0; i < b.length(); i++){
+            int c = (int)  b.charAt(i);
+            letters[c]--;
+            if(letters[c]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+    // Why does this work? We make very good use of the fact the strings are of the same size
+    // So, if they are not permutations of each other this means: the second string has a letter the first does not have accounted for
+    // which will force letters[c] to go below 0
+    // and since the strings are of the same size, there will never be a case of "left over" aka slots of letters[c] that are above 0
+
+    // Runtime: O(n)
+    // Space complexity: O(1). Just needed an array of size 128, that's a constant size!
 
 }
--- Python/Data Structures/Arrays and Strings/1.1.py
+++ Python/Data Structures/Arrays and Strings/1.1.py
@@ -1,18 +1,21 @@
-#Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+# Implement an algorithm to determine if a string has all unique characters.
+# What if you cannot use additional data structures?
 # assumption: assume uppercase and lowercase are different characters
 def has_unique_chars(str):
-	counts = {} # dictionary to see if we have seen the character already
-	for i in range(0, len(str)):
-		char = str[i]
-		if char in counts:
-			return False
-		counts[char] = True
-	return True
-		
-	
-print has_unique_chars("HIiiH")
+    counts = {}  # dictionary to see if we have seen the character already
+    for i in range(0, len(str)):
+        char = str[i]
+        if char in counts:
+            return False
+        counts[char] = True
+    return True
+
+
+print
+has_unique_chars("HIiiH")
 assert has_unique_chars("HIiiH") == False
-print has_unique_chars("HiI")
+print
+has_unique_chars("HiI")
 assert has_unique_chars("HiI") == True
 
 '''
@@ -21,29 +24,32 @@ Time Complexity: O(n), since in the worst case we need to look at the entire str
 Space Complexity: O(n), to store all the unqiue chars in our counts dictionary
 '''
 
-#Improved solution (from book): there are only a finite amount of different characters that exist. 
-#So while the string may be arbitrarily large, we only need a O(1) extra space to check if characters are duplicated
+# Improved solution (from book): there are only a finite amount of different characters that exist.
+# So while the string may be arbitrarily large, we only need a O(1) extra space to check if characters are duplicated
 
-#Assume 256 possible unique characters (ASCII)
-#function ord() would get the int value of the char. 
-#And in case you want to convert back after playing with the number, function chr() does the trick.	
+# Assume 256 possible unique characters (ASCII)
+# function ord() would get the int value of the char.
+# And in case you want to convert back after playing with the number, function chr() does the trick.
 def has_unique_chars_2(str):
-	if len(str) > 256:
-		return False	
-	char_set = [False for i in range(256)] # list comprehension, for creating a boolean array all initialized to false
-	for i in range(0, len(str)):
-		char_val = ord(str[i])
-		if(char_set[char_val]):
-			return False
-		char_set[char_val] = True
-	return True
+    if len(str) > 256:
+        return False
+    char_set = [False for i in range(256)]  # list comprehension, for creating a boolean array all initialized to false
+    for i in range(0, len(str)):
+        char_val = ord(str[i])
+        if (char_set[char_val]):
+            return False
+        char_set[char_val] = True
+    return True
 
-print("=====")	
-print has_unique_chars_2("HIiiH")
+
+print("=====")
+print
+has_unique_chars_2("HIiiH")
 assert has_unique_chars_2("HIiiH") == False
-print has_unique_chars_2("HiI")
+print
+has_unique_chars_2("HiI")
 assert has_unique_chars_2("HiI") == True
-	
+
 '''
 Input is a string of length n
 Time Complexity: O(n), since in the worst case we need to look at the entire string 
@@ -56,18 +62,21 @@ Space Complexity: O(1), to count duplicate chars, we only need storage for all t
 #Have to compare each char with every other char.
 
 def has_unique_chars_extra(str):
-	for i in range(0, len(str)):
-		curr_char = str[i]
-		for j in range(i+1, len(str)):
-			compare_char = str[j]
-			if(curr_char == compare_char):
-				return False
-	return True
-	
-print("=====")	
-print has_unique_chars_extra("HIiiH")
+    for i in range(0, len(str)):
+        curr_char = str[i]
+        for j in range(i + 1, len(str)):
+            compare_char = str[j]
+            if (curr_char == compare_char):
+                return False
+    return True
+
+
+print("=====")
+print
+has_unique_chars_extra("HIiiH")
 assert has_unique_chars_extra("HIiiH") == False
-print has_unique_chars_extra("HiI")
+print
+has_unique_chars_extra("HiI")
 assert has_unique_chars_extra("HiI") == True
 
 '''
