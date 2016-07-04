85a50e32eea8af789de65ebf990658ba89174b49
eidehua <edthemaster1@gmail.com>
2016-07-03 22:51:00 -0700

    start 2.1

---
+++ Python/Data Structures/Linked Lists/2.1_Remove_Dups.py
@@ -0,0 +1,8 @@
+# given an unsorted linked list, remove the duplicates
+# eg: 5->4->3->5 
+# after remove dup: 5->4->3
+
+
+
+
+# follow up: how would you solve this problem if a temporary buffer is not allowed
