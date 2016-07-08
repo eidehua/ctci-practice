96016df3ac5dd94663272a9b80fc005bc0dded70
Edward Xue <edthemaster1@gmail.com>
2016-07-07 22:25:57 -0700

    Learning basics of bits (SHOULD BE MEMORIZED). Later I need to learn when to apply bit manipulation.

---
+++ Java/Concepts_and_Algorithms/Bit_Manipulation/P5_1_Insertion.java
@@ -0,0 +1,17 @@
+/* You are given two 32 bit numbers N and M and two bit positions, i and j
+   write a method to insert M into N such that M starts at bit j and ends at bit i
+   you can assume that the bits j through I have enough space to fit all of M
+   Example
+   input N = 10000000000, M = 10011, i = 2, j= 6
+   output N = 10001001100
+*/
+public class P5_1_Insertion {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int inserter(int large_bit, int small_bit, int i, int j) {
+        // checks: large_bit should be larger than small_bit, so small_bit can be inserted in between large_bit
+    }
+}
